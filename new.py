c9676f9c64deaa890bec86cdc0beb26bff9b6d40
Pooja bhavani <147735975+pooja-bhavani@users.noreply.github.com>
2025-05-03 10:20:09 +0530

    Create new.py

---
+++ new.py
@@ -0,0 +1,6 @@
+Difference between centralized and distributed version control?
+* CVC: All version history and files are stored on a central server. Does’t have local storage to store copy of the data pushed to central repository. 
+Developers pull the latest version and commit changes directly to the central repository.
+
+* DVCS: Every developer's machine has a full local copy of the entire repository, including the complete version history. 
+Developers can commit, revert, and review history locally, even without internet access. Changes are later pushed to or pulled from a central/shared repository to collaborate with others.
